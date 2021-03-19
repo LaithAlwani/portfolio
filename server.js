49f118695ecbf001e1086948c51c5407ba59728a
Laith Alwani <laithalwani@gmail.com>
2021-03-19 14:55:13 -0400

    email notification feature added for contact form

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const app = express();
 const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 8080;
 
+const app = express();
 //express config
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -20,10 +21,36 @@ app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.post("/submit", (req, res) => {
+app.post("/email", (req, res) => {
   const inquiry = req.body;
-  //send the values via email.
-  res.redirect("/#contact");
+  console.log(inquiry);
+ 
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "breanne.schmeler85@ethereal.email", // generated ethereal user
+      pass: "6hjKadEGGDVY1vsZug" // generated ethereal password
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email, // sender address
+    to: "breanne.schmeler85@ethereal.email", // list of receivers
+    subject: "Request", // Subject line
+    text: req.body.message, // plain text body
+   
+  }
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, response)=>{
+    if(error){
+      res.status(500).json("Internal server error")
+    }else{
+      res.json("Email sent!!")
+    }
+  });
+  res.redirect("/");
 });
 
 app.get("/", (req, res) => {
